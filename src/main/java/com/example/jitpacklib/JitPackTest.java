9182bcdaecb295225d5be53116ebbf9b2e137e14
mrchenjialiang <jiujiebinghuang007@gmail.com>
2022-02-10 18:25:35 +0800

    初始
    初始

---
+++ src/main/java/com/example/jitpacklib/JitPackTest.java
@@ -0,0 +1,18 @@
+package com.example.jitpacklib;
+
+import android.util.Log;
+
+/**
+ * author:ChenJiaLiang
+ * Date:2022/2/10
+ * Description:测试
+ */
+public class JitPackTest {
+
+    /**
+     * 测试api
+     */
+    public static void test() {
+        Log.e("JitPack", "Test");
+    }
+} 
